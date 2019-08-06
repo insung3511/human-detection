2cc65eba27a708867f320dc32e98995aa8ccc2dc
insung3511 <insung.park123@gmail.com>
2019-08-06 14:00:32 +0900

    후아후아후아
    김연규 일해라

--- t_main.py
+++ t_main.py
@@ -6,21 +6,20 @@ from imutils import paths
 from imutils.object_detection import non_max_suppression
 
 start = time.time()
-hog = cv2.HOGDescriptor()
-hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
-i = 0
+def get_num_of_people(imagePath):
+    hog = cv2.HOGDescriptor()
+    hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+    
+    i = 0
 
-for imagePath in paths.list_images('./test'):
     image = cv2.imread(imagePath)
     image = imutils.resize(image, width=min(400, image.shape[1]))
     orig = image.copy()
-
     (rects, weights) = hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
 
     rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
     pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
-
     for (xA, yA, xB, yB) in pick:
         cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)
         cv2.imwrite('result.jpg', image)
@@ -28,5 +27,6 @@ for imagePath in paths.list_images('./test'):
     print("----------")
 
     filename = imagePath[imagePath.rfind("/") + 1:] 
-print("time : ", time.time() - start)
-print(i)
+    if not i == 0:
+        return True
+    return False
